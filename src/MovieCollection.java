16807acf05893529ad1ed7e6be93020b51c3a21a
latenight-aluna <lusz0001@stud.kea.dk>
2024-02-20 15:59:45 +0100

    Projekt

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,7 +1,12 @@
 public class MovieCollection {
     private Movie[] movies;
+    int count = 0;
 
     public MovieCollection() {
         this.movies = new Movie[5];
+    }
+    public void addMovie(String titel, String director, int yearCreated, boolean isInColor, int lengthInMinutes,
+                           String genre) {
+        movies[count++] = new Movie(titel, director, yearCreated, isInColor, lengthInMinutes, genre);
     }
 }
