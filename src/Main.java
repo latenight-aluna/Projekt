cd2b58b22f1183a89bfa9c675740b59d3d45a3de
latenight-aluna <lusz0001@stud.kea.dk>
2024-02-20 15:42:14 +0100

    Projekt

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Vælg en film");
+        String userInput = scanner.nextLine();
+
+        System.out.println(userInput);
+    }
+}
