5438849d01d4c5802ac2eade97f815832e4bc99a
latenight-aluna <lusz0001@stud.kea.dk>
2024-02-21 11:03:44 +0100

    Zakka's kode

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,34 @@
 import java.util.Scanner;
-
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Vælg en film");
-        String userInput = scanner.nextLine();
+        MovieCollection myCollection = new MovieCollection();
 
-        System.out.println(userInput);
+        System.out.println("To add a movie to your movie collection, follow the instructions below.");
+
+        System.out.println("Movie title:");
+        String title = scanner.nextLine();
+
+        System.out.println("Director name:");
+        String director = scanner.nextLine();
+
+        System.out.println("Release year:");
+        int yearCreated = scanner.nextInt();
+
+        System.out.println("Is the movie in color? (type true for yes or false for no):");
+        boolean isInColor = scanner.nextBoolean();
+
+        System.out.println("Movie length in minutes:");
+        int lengthInMinutes = scanner.nextInt();
+
+        System.out.println("Movie genre:");
+        String genre = scanner.next();
+
+        myCollection.addMovie(title, director, yearCreated, isInColor, lengthInMinutes, genre);
+
+        System.out.println("Movie successfully added to your movie collection.");
     }
 }
